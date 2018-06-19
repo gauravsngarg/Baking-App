34a1fed02b2a215b9a5f31783e8cbc2782edf34d
Gaurav Garg <gauravsngarg@gmail.com>
2018-06-19 20:42:02 +0530

    Add model to store Ingredients
    Recipe Ingredients

---
+++ app/src/main/java/gauravsngarg/com/myapplication/model/RecipeIngredients.java
@@ -0,0 +1,39 @@
+package gauravsngarg.com.myapplication.model;
+
+/**
+ * Created by GG on 19/06/18.
+ */
+
+public class RecipeIngredients {
+//    quantity: 2,
+//    measure: "CUP",
+//    ingredient: "Graham Cracker crumbs"
+
+    int quantity;
+    String measure;
+    String ingredient;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+}
