75bd1b3782d0a282e6ee991816f49bb88c20a9fa
Gaurav Garg <gauravsngarg@gmail.com>
2018-08-01 21:49:51 +0530

    Merge MainActivity Changes

--- app/src/main/java/gauravsngarg/com/bakingrecipes/MainActivity.java
+++ app/src/main/java/gauravsngarg/com/bakingrecipes/MainActivity.java
@@ -1,15 +1,22 @@
 package gauravsngarg.com.bakingrecipes;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Master_Recipe_Fragment.OnFragmentInteractionListener{
 
-    private boolean mTwoPane;
+    // private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
