5d4f652a4ce6505c93cfe70060248c0f44e279ee
Gaurav Garg <gauravsngarg@gmail.com>
2018-08-16 06:50:40 +0530

    Add RecyclerView CLick Listener
    Add ListView onClick functionality

--- app/src/main/java/gauravsngarg/com/bakingrecipes/MainActivity.java
+++ app/src/main/java/gauravsngarg/com/bakingrecipes/MainActivity.java
@@ -1,10 +1,15 @@
 package gauravsngarg.com.bakingrecipes;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements Recipe_List_Fragment.OnFragmentInteractionListener{
+import gauravsngarg.com.bakingrecipes.adapter.RecipeAdapter;
+
+public class MainActivity extends AppCompatActivity implements Recipe_List_Fragment.OnFragmentInteractionListener,
+        RecipeAdapter.RecipeListItemClickListener{
 
     // private boolean mTwoPane;
 
@@ -13,10 +18,25 @@ public class MainActivity extends AppCompatActivity implements Recipe_List_Fragm
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//       FragmentManager fragmentManager = getFragmentManager();
+//       Recipe_List_Fragment fragment = new Recipe_List_Fragment();
+//       FragmentTransaction fragmentTransaction =  fragmentManager.beginTransaction();
+//       fragmentTransaction.add(R.id.container,fragment );
+//       fragmentTransaction.commit(); // save the changes
+
+
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public void onRecipeListItemClick(int clickedItemIndex) {
+        Toast.makeText(this, "Item Clicked" + (clickedItemIndex+1),Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, RecipeDetails.class);
+        startActivity(i);
     }
 }
