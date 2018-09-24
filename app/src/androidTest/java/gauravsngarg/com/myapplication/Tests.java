677cddde86ed2e14badc59b6f72c3a9ea48fc37c
Gaurav Garg <gauravsngarg@gmail.com>
2018-09-25 00:06:41 +0530

    Add Espresso Tests

--- app/src/androidTest/java/gauravsngarg/com/myapplication/Tests.java
+++ app/src/androidTest/java/gauravsngarg/com/myapplication/Tests.java
@@ -19,7 +19,10 @@ import gauravsngarg.com.bakingrecipes.R;
 import gauravsngarg.com.bakingrecipes.activities.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
@@ -36,9 +39,9 @@ public class Tests {
     public void tests() {
         ViewInteraction frameLayout = onView(
                 allOf(childAtPosition(
-                        allOf(withId(R.id.recycler_view_recipes),
+                        allOf(withId(R.id.master_list_fragment),
                                 childAtPosition(
-                                        withId(R.id.coordinator),
+                                        withId(R.id.frame),
                                         0)),
                         0),
                         isDisplayed()));
@@ -51,66 +54,16 @@ public class Tests {
 
         frameLayout.check(matches(isDisplayed()));
 
-       /*ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.recipes_recycler_view),
-                        childAtPosition(
-                                withId(R.id.coordinator),
-                                0)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
+        onView(allOf(withId(R.id.recycler_view_recipes))).perform(actionOnItemAtPosition(0, click()));
 
-        ViewInteraction cardView = onView(
-                allOf(withId(R.id.ingredient_cv),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                0),
-                        isDisplayed()));
-        cardView.perform(click());
-
-        ViewInteraction linearLayout = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.ingredients_recycler_view),
-                                0),
-                        0),
-                        isDisplayed()));
-        linearLayout.check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.item_list))).perform(actionOnItemAtPosition(0, click()));
 
         pressBack();
 
-        ViewInteraction frameLayout2 = onView(
-                allOf(withId(R.id.ingredient_cv),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                0),
-                        isDisplayed()));
-        frameLayout2.check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.item_list))).perform(actionOnItemAtPosition(1, click()));
 
-        ViewInteraction frameLayout3 = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.steps_recycle_view),
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1)),
-                        0),
-                        isDisplayed()));
-        frameLayout3.check(matches(isDisplayed()));
-
-        ViewInteraction recyclerView2 = onView(
-                allOf(withId(R.id.steps_recycle_view),
-                        childAtPosition(
-                                withClassName(is("android.widget.LinearLayout")),
-                                1)));
-        recyclerView2.perform(actionOnItemAtPosition(0, click()));
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(10000);
+            Thread.sleep(8000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -119,31 +72,11 @@ public class Tests {
                 allOf(childAtPosition(
                         allOf(withId(R.id.exo_content_frame),
                                 childAtPosition(
-                                        withId(R.id.video_view),
+                                        withId(R.id.playerView),
                                         0)),
                         0),
                         isDisplayed()));
         view.check(matches(isDisplayed()));
-
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.long_description_tv), withText("Recipe Introduction"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class),
-                                        0),
-                                2),
-                        isDisplayed()));
-        textView.check(matches(withText("Recipe Introduction")));
-
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.long_description_tv), withText("Recipe Introduction"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class),
-                                        0),
-                                2),
-                        isDisplayed()));
-        textView2.check(matches(withText("Recipe Introduction")));*/
 
     }
 
